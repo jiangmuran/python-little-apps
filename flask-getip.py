a95e8232d5dac7a9518e4ce73f28f44d16abc403
jiangmuran <wd.jiang@foxmail.com>
2023-02-02 18:35:09 +0800

    Create flask-getip.py

---
+++ flask-getip.py
@@ -0,0 +1,32 @@
+from flask import Flask,request
+
+userlist = []
+
+jumptext='跳转失败，您可能开启了代理或者出现了未知错误'
+
+app = Flask(__name__)
+
+@app.route('/get/')
+def adminget():
+	return str(userlist)
+
+@app.route('/clear/')
+def clear():
+	global userlist
+	userlist=[]
+	return '404 not found'
+
+@app.route('/changetext/')
+def changetext():
+	jumptext=request.args.get('text')
+	return '修改成功！'
+
+@app.route('/jump/')
+def jumpa():
+	id = request.args.get('jumpid')
+	ip = request.remote_addr
+	userlist.append({'id':id,'ip':ip})
+	return jumptext
+
+if __name__ == '__main__':
+	app.run(debug=True,port=80)
