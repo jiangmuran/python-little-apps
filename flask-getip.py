a92ef37a73024a99763c04696744cfbc4819ad07
jiangmuran <wd.jiang@foxmail.com>
2023-02-03 09:46:59 +0800

    Update flask-getip.py

--- flask-getip.py
+++ flask-getip.py
@@ -27,7 +27,7 @@ def changetext():
 def jumpa():
 	id = request.args.get('jumpid')
 	ip = request.remote_addr
-	userlist.append({'id':id,'ip':ip,'time':str(time.time)})
+	userlist.append({'id':id,'ip':ip,'time':str(int(time.time()*1000))})
 	return jumptext
 
 if __name__ == '__main__':
